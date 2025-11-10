b752ba88d59bf894fda60a907162525736e7cbc9
DevopsChandraTech <devopschandra42@gmail.com>
2025-11-10 14:52:26 +0530

    Delete terraform-vpc directory

--- terraform-vpc/locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-    common_name_suffix = "${var.project_name}-${var.environment}" #roboshop-dev-branch
-    common_tags = {
-        Terraform = true
-        Project = "roboshop"
-    }
-}
-
