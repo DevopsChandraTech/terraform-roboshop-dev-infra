287e59e728a2565912ae7c316c9eec80520bb1ab
DevOpsChandraTech <devopschandra42@gmail.com>
2025-12-10 15:47:19 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -57,10 +57,11 @@ resource "aws_ami_from_instance" "catalogue" {
 
 resource "aws_lb_target_group" "catalogue" {
   name     = "${local.common_name_suffix}-catalogue"
-  port     = 80
+  port     = 8080
   protocol = "HTTP"
   vpc_id   = data.aws_ssm_parameter.vpc_id.value
-  deregistration_delay = 60 # waiting for instance deletion then instance deleted 
+  deregistration_delay = 60 # waiting for instance deletion then instance deleted
+  
   health_check {
     enabled   = true
     healthy_threshold = 2
