8d1b9f04f906c0ed0216be23d42041306a28570d
DevOpsChandraTech <devopschandra42@gmail.com>
2025-12-10 15:39:42 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -16,7 +16,6 @@ resource "terraform_data" "catalogue" { #if instance id replaced it can be trigg
       aws_instance.catalogue.id
   ] 
 
-
   connection {
     type        = "ssh"
     user        = "ec2-user"
@@ -60,7 +59,7 @@ resource "aws_lb_target_group" "catalogue" {
   name     = "${local.common_name_suffix}-catalogue"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = local.vpc_id
+  vpc_id   = data.aws_ssm_parameter.vpc_id.value
   deregistration_delay = 60 # waiting for instance deletion then instance deleted 
   health_check {
     enabled   = true
