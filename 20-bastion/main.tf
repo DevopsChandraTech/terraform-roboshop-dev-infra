4afe3e9e9a0faf83018a522dab749d7bed705e8d
DevOpsChandraTech <devopschandra42@gmail.com>
2025-12-10 15:03:21 +0530

    terraform

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -4,6 +4,12 @@ resource "aws_instance" "terraform" {
   vpc_security_group_ids = [local.bastion_sg_id]
   subnet_id = local.public_subnet_id
   iam_instance_profile = aws_iam_instance_profile.bastion.name
+  #increase storage on bastion host
+  # root_block_device {
+  #   volume_size = 50 # Increase to 50 GB, for example
+  #   volume_type = "gp2" # Or "gp3" for better performance and cost
+  #   encrypted   = true
+  # }
   
   user_data = file("bastion.sh")
   tags = merge(
