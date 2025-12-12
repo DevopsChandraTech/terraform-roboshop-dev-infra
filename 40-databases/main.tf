91befe58c83beb11f008bec26a7135289f1e5a76
DevOpsChandraTech <devopschandra42@gmail.com>
2025-12-12 23:20:12 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -40,7 +40,7 @@ resource "terraform_data" "mongodb" { #if instance id replaced it can be trigger
   }
 }
 
-/* #redis:
+#redis:
 resource "aws_instance" "redis" {
   ami           = local.ami_id
   instance_type = "t3.micro"
@@ -169,7 +169,7 @@ resource "terraform_data" "mysql" { #if instance id replaced it can be triggered
       "sudo sh /tmp/bootstrap.sh mysql dev"
     ]
   }
-} */
+}
 
 resource "aws_route53_record" "mongodb" {
   zone_id = var.zone_id
@@ -180,7 +180,7 @@ resource "aws_route53_record" "mongodb" {
   allow_overwrite = true
 }
 
-/* resource "aws_route53_record" "mysql" {
+resource "aws_route53_record" "mysql" {
   zone_id = var.zone_id
   name    = "mysql-${var.environment}.${var.domain_name}" #mysql-dev.devaws.shop
   type    = "A"
@@ -205,4 +205,4 @@ resource "aws_route53_record" "redis" {
   ttl     = 1
   records = [aws_instance.redis.private_ip]
   allow_overwrite = true
-} */
+}
